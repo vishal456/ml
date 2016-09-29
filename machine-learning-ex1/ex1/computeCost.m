54eca70d6a25de5bc96c64ba96ea028d6e642d52
vishal kumawat <vishal.kumawat@tracxn.com>
2016-09-29 15:27:52 +0530

    [Master] : Completed ex1

--- machine-learning-ex1/ex1/computeCost.m
+++ machine-learning-ex1/ex1/computeCost.m
@@ -13,10 +13,7 @@ J = 0;
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
-
-
-
-
+M = X*theta - y;
+J = (transpose(M)*M)/(2*m);
 % =========================================================================
-
 end
